82dac6956c981f39dfc32a19d7a7a98d52420d29
hrfmmymt <hrfmmymt@gmail.com>
2016-05-11 00:28:56 +0900

    es2015記法、拡張子変更、gulpfile編集

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,18 +1,20 @@
 'use strict';
 
 if (window.addEventListener) {
-  var pressed = [];
-  var konamiCode = '38,38,40,40,37,39,37,39,66,65';
-  window.addEventListener('keydown', function (k) {
-    pressed.push(k.keyCode);
-    if (pressed.toString().indexOf(konamiCode) >= 0) {
-      var el = document.querySelector('body');
-      if (el.className.match(/(?:^|\s)konamied(?!\S)/)) {
-        el.className = el.className.replace(/(?:^|\s)konamied(?!\S)/g, '');
-      } else {
-        el.className += ' konamied';
+  (function () {
+    var pressed = [];
+    var konamiCode = '38,38,40,40,37,39,37,39,66,65';
+    window.addEventListener('keydown', function (k) {
+      pressed.push(k.keyCode);
+      if (pressed.toString().indexOf(konamiCode) >= 0) {
+        var el = document.querySelector('body');
+        if (el.className.match(/(?:^|\s)konamied(?!\S)/)) {
+          el.className = el.className.replace(/(?:^|\s)konamied(?!\S)/g, '');
+        } else {
+          el.className += ' konamied';
+        }
+        pressed = [];
       }
-      pressed = [];
-    }
-  }, true);
+    }, true);
+  })();
 }
--- src/js/script.es6
+++ src/js/script.es6
@@ -1,10 +1,10 @@
 if (window.addEventListener) {
-  var pressed = [];
-  var konamiCode = '38,38,40,40,37,39,37,39,66,65';
-  window.addEventListener('keydown', function (k) {
+  let pressed = [];
+  let konamiCode = '38,38,40,40,37,39,37,39,66,65';
+  window.addEventListener('keydown', (k) => {
     pressed.push(k.keyCode);
     if (pressed.toString().indexOf(konamiCode) >= 0) {
-      var el = document.querySelector('body');
+      let el = document.querySelector('body');
       if (el.className.match(/(?:^|\s)konamied(?!\S)/)) {
         el.className = el.className.replace(/(?:^|\s)konamied(?!\S)/g, '');
       } else {
