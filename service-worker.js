cf13ed1ada10921b8b0e5d372dc3e81917f09ea7
hrfmmymt <hrfmmymt@gmail.com>
2017-10-31 11:54:55 +0900

    Source organize

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,13 @@
-const cacheName = '0.0.1';
+const cacheName = '0.0.2';
 const filesToCache = [
   '/',
   '/index.html',
-  '//pbs.twimg.com/profile_images/672293869120163840/7q4wxHvu.jpg'
+  '/img/profile.jpg'
 ];
 
-self.addEventListener('install', function(e) {
+self.addEventListener('install', event => {
   console.log('[ServiceWorker] Install');
-  e.waitUntil(
+  event.waitUntil(
     caches.open(cacheName).then(function(cache) {
       console.log('[ServiceWorker] Caching app shell');
       return cache.addAll(filesToCache);
@@ -15,11 +15,11 @@ self.addEventListener('install', function(e) {
   );
 });
 
-self.addEventListener('fetch', function(e) {
-  console.log('[ServiceWorker] Fetch', e.request.url);
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
+self.addEventListener('fetch', event => {
+  console.log('[ServiceWorker] Fetch', event.request.url);
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
     })
   );
 });
